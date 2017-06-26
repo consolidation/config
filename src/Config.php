9818154d78527da60cfe125401827bbb8edf3e99
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-06-26 14:19:25 -0700

    Remove some Robo-specfic constructs.

--- src/Config.php
+++ src/Config.php
@@ -5,12 +5,6 @@ use Dflydev\DotAccessData\Data;
 
 class Config
 {
-    const PROGRESS_BAR_AUTO_DISPLAY_INTERVAL = 'progress-delay';
-    const DEFAULT_PROGRESS_DELAY = 2;
-    const SIMULATE = 'simulate';
-    const INTERACTIVE = 'interactive';
-    const DECORATED = 'decorated';
-
     /**
      * @var Data
      */
@@ -28,7 +22,7 @@ class Config
     public function __construct(array $data = null)
     {
         $this->config = new Data($data);
-        $this->defaults = $this->getGlobalOptionDefaultValues();
+        $this->defaults = [];
     }
 
     /**
@@ -177,23 +171,6 @@ class Config
     }
 
     /**
-     * Return an associative array containing all of the global configuration
-     * options and their default values.
-     *
-     * @return array
-     */
-    public function getGlobalOptionDefaultValues()
-    {
-        $globalOptions =
-        [
-            self::PROGRESS_BAR_AUTO_DISPLAY_INTERVAL => self::DEFAULT_PROGRESS_DELAY,
-            self::SIMULATE => false,
-        ];
-
-        return $globalOptions;
-    }
-
-    /**
      * Return the default value for a given configuration item.
      *
      * @param string $key
@@ -231,69 +208,5 @@ class Config
     {
         $this->defaults[$key] = $value;
         return $this;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isSimulated()
-    {
-        return $this->get(self::SIMULATE);
-    }
-
-    /**
-     * @param bool $simulated
-     *
-     * @return $this
-     */
-    public function setSimulated($simulated = true)
-    {
-        return $this->set(self::SIMULATE, $simulated);
-    }
-
-    /**
-     * @return bool
-     */
-    public function isInteractive()
-    {
-        return $this->get(self::INTERACTIVE);
-    }
-
-    /**
-     * @param bool $simulated
-     *
-     * @return $this
-     */
-    public function setInteractive($interactive = true)
-    {
-        return $this->set(self::INTERACTIVE, $interactive);
-    }
-
-    /**
-     * @return bool
-     */
-    public function isDecorated()
-    {
-        return $this->get(self::DECORATED);
-    }
-
-    /**
-     * @param bool $decorated
-     *
-     * @return $this
-     */
-    public function setDecorated($decorated = true)
-    {
-        return $this->set(self::DECORATED, $decorated);
-    }
-
-    /**
-     * @param int $interval
-     *
-     * @return $this
-     */
-    public function setProgressBarAutoDisplayInterval($interval)
-    {
-        return $this->set(self::PROGRESS_BAR_AUTO_DISPLAY_INTERVAL, $interval);
     }
 }
