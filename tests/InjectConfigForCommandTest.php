ada0ccce7acb6c7f1587f9d5b469703c23486708
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-06-27 16:05:32 -0700

    Add a test for InjectConfigForCommand

---
+++ tests/InjectConfigForCommandTest.php
@@ -0,0 +1,98 @@
+<?php
+namespace Consolidation\Config\Command;
+
+use Consolidation\Config\Config;
+use Consolidation\TestUtils\MyFooCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InjectConfigForCommandTest extends \PHPUnit_Framework_TestCase
+{
+    protected $config;
+
+    protected function setUp()
+    {
+        $data = [
+            // Define some configuration settings for the options for
+            // the commands my:foo and my:bar.
+            'command' => [
+                'my' => [
+                    // commands.my.options.* apply to all my:* commands.
+                    'options' => [
+                        'dir' => '/etc/common',
+                        'priority' => 'normal',
+                    ],
+                    'foo' => [
+                        // commands.my.foo.options.* apply only to the my:foo command.
+                        'options' => [
+                            'name' => 'baz',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $this->config = new Config($data);
+    }
+
+    public function testInjection()
+    {
+        $command = new MyFooCommand();
+        $input = new StringInput('my:foo');
+
+        [$status, $output] = $this->runCommandViaApplication($command, $input);
+
+        $expectedOutput = <<< EOT
+Enter my:foo
+dir: /etc/common
+name: baz
+other: fish
+EOT;
+
+        $this->assertEquals(0, $status);
+        $this->assertEquals($expectedOutput, $output);
+    }
+
+    public function testInjectionWithOverride()
+    {
+        $command = new MyFooCommand();
+        $input = new StringInput('my:foo --name=Fred');
+
+        [$status, $output] = $this->runCommandViaApplication($command, $input);
+
+        $expectedOutput = <<< EOT
+Enter my:foo
+dir: /etc/common
+name: Fred
+other: fish
+EOT;
+
+        $this->assertEquals(0, $status);
+        $this->assertEquals($expectedOutput, $output);
+    }
+
+    protected function runCommandViaApplication($command, $input)
+    {
+        $configInjector = new InjectConfigForCommand($this->config);
+
+        $output = new BufferedOutput();
+        $application = new Application('TestApplication', '0.0.0');
+
+        $eventDispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
+        $eventDispatcher->addSubscriber($configInjector);
+        $application->setDispatcher($eventDispatcher);
+
+        $application->setAutoExit(false);
+        $application->add($command);
+
+        $statusCode = $application->run($input, $output);
+        $commandOutput = trim($output->fetch());
+
+        return [$statusCode, $commandOutput];
+    }
+}
