c5fc8ae8ca813ba6bba0556d00c0bafd12a5e718
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-07-28 10:26:06 -0700

    Inject option defaults from configuration into help.

--- src/Inject/ConfigForCommand.php
+++ src/Inject/ConfigForCommand.php
@@ -7,14 +7,22 @@ use Consolidation\Config\Util\ConfigFallback;
 use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Console\Application;
 
 class ConfigForCommand implements EventSubscriberInterface
 {
     protected $config;
+    protected $application;
 
     public function __construct(ConfigInterface $config)
     {
         $this->config = $config;
+    }
+
+    public function setApplication(Application $application)
+    {
+        $this->application = $application;
+
     }
 
     /**
@@ -35,6 +43,16 @@ class ConfigForCommand implements EventSubscriberInterface
     public function injectConfiguration(ConsoleCommandEvent $event)
     {
         $command = $event->getCommand();
+        $this->injectConfigurationForCommand($command);
+
+        $targetOfHelpCommand = $this->getHelpCommandTarget($command, $event->getInput());
+        if ($targetOfHelpCommand) {
+            $this->injectConfigurationForCommand($targetOfHelpCommand);
+        }
+    }
+
+    protected function injectConfigurationForCommand($command)
+    {
         $commandName = $command->getName();
         $commandName = str_replace(':', '.', $commandName);
         $definition = $command->getDefinition();
@@ -47,5 +65,32 @@ class ConfigForCommand implements EventSubscriberInterface
                 $inputOption->setDefault($value);
             }
         }
+    }
+
+    protected function getHelpCommandTarget($command, $input)
+    {
+        if (($command->getName() != 'help') || (!isset($this->application))) {
+            return false;
+        }
+
+        // Symfony 3.x prepares $input for us; Symfony 2.x, on the other
+        // hand, passes it in prior to binding with the command definition,
+        // so we have to go to a little extra work.  It may be inadvisable
+        // to do these steps for commands other than 'help'.
+        if (!$input->hasArgument('command_name')) {
+            $command->ignoreValidationErrors();
+            $command->mergeApplicationDefinition();
+            $input->bind($command->getDefinition());
+        }
+
+        // Symfony Console helpfully swaps 'command_name' and 'command'
+        // depending on whether the user entered `help foo` or `--help foo`.
+        // One of these is always `help`, and the other is the command we
+        // are actually interested in.
+        $nameOfCommandToDescribe = $input->getArgument('command_name');
+        if ($nameOfCommandToDescribe == 'help') {
+            $nameOfCommandToDescribe = $input->getArgument('command');
+        }
+        return $this->application->find($nameOfCommandToDescribe);
     }
 }
